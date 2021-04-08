797f3b973f90ad9d715e925fd5046f9089f16d77
unknown <edward2000ed@gmail.com>
2021-04-08 08:27:50 +0530

    adding binary tree initial commit

---
+++ binaryTree in console/sketch.js
@@ -0,0 +1,17 @@
+var Tree;
+
+function setup() {
+
+    noCanvas();
+    tree = new Tree();
+
+    for(let i = 0; i < 10; i++) {
+       tree.addValue(floor(random(0, 100))); 
+    }
+
+    console.log(tree);
+    tree.traverse();
+}
+
+
+
---
+++ binaryTree in console/tree.js
@@ -0,0 +1,22 @@
+
+function Tree() {
+    this.root = null;
+}
+
+Tree.prototype.addValue = function(val) {
+    var n = new Node(val);
+    if(this.root == null) {
+        this.root = n;
+    } else {
+        this.root.addNode(n);
+    }
+}
+
+Tree.prototype.traverse = function()
+{
+    this.root.visit();
+}
+
+Tree.prototype.search = function(val) {
+    return this.root.search(val);
+}
